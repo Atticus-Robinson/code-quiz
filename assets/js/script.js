1f3184523d9c4d9c5d085322bc1bddece1891ff7
Atticus Robinson <acerobinson2@gmail.com>
2022-01-16 19:11:39 -0600

    final code run-through

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,16 @@ var count = 0;
 var score;
 
 
-//Create question
+//Create question and answer in quiz area 
 function createQA() {
+    
+    //Create ul element and append
     var ulE1 = document.createElement("ul");
     ulE1.className = "qa-list";
     ulE1.setAttribute("id", "qa");
     areaE1.appendChild(ulE1);
 
-
+    //Create question li element and append
     var listItemE1 = document.createElement("li");
     listItemE1.className = "list-item";
     listItemE1.textContent = pullQ(columnCounter);
@@ -20,9 +22,11 @@ function createQA() {
     listItemE1.setAttribute("qa", "q");
     ulE1.appendChild(listItemE1);
 
+    //Create answers and shuffle
     let answers = pullA(columnCounter);
     new_obj = shuffleA(answers);
 
+    //Create answer li elements and append
     for (x in new_obj) {
         var listItemE2 = document.createElement("li");
         listItemE2.className = "list-item";
@@ -34,15 +38,19 @@ function createQA() {
 
 }
 
+//Upon clicking an element in the quiz area
 quizarea.onclick = function(event) {
     var className = event.target.className;
     
+    //Check target div
     if (className === "start-button" || className === "start-text") {
+        //If target was start button, remove start button or scorelist, create timer, and start questions
         removeItems('start-button');
         removeItems("scorelist");
         timerBox();
         createQA();
     } else if (className === "list-item" ) {
+        //If target was a qa list item, get text content, check against answer array with checkAnswer()
         var qa = event.target.getAttribute("qa");
         if (qa === 'q') return;
         var answerText = event.target.textContent;
@@ -51,9 +59,9 @@ quizarea.onclick = function(event) {
     }
 }
 
-//Check answer; text from clicked response vs corresponding checkArray
+//Check answer text from clicked response vs corresponding checkArray
 function checkAnswer(text) {
-    //If the text matches array value, new question
+    //If the text matches array value, move column counter. If there are more questions in the array generate next question. If not end game
     if (text === checkArray[columnCounter]) {
         columnCounter++;
         
@@ -70,6 +78,7 @@ function checkAnswer(text) {
     }
 }
 
+//Taking an idName, get the idName, and remove element
 function removeItems(idName) {
     var rem = document.getElementById(idName);
     if (rem){
@@ -78,6 +87,7 @@ function removeItems(idName) {
     return;
 }
 
+//Pull answers from answer arrays, used to pull all answers in a question column
 function pullA(questionIndex) {
     let a1 = answerArray[questionIndex],
         a2 = answerArray2[questionIndex],
@@ -91,6 +101,7 @@ function pullA(questionIndex) {
     };
 }
 
+//Pull questions from question array, if its the first question (column counter 0) then shuffle the array. If not pull from current column and return question
 function pullQ(localCounter) {
     if (columnCounter === 0) {
         shuffle(questionArray, answerArray, answerArray2, answerArray3, answerArray4, checkArray)
@@ -99,6 +110,7 @@ function pullQ(localCounter) {
     return retQ;
 }
 
+//Generate start button, set column counter to 0 for quiz restart
 function startButton() {
     columnCounter = 0;
     var startButtonE1 = document.createElement("button");
@@ -108,6 +120,7 @@ function startButton() {
     areaE1.appendChild(startButtonE1);
 }
 
+//Genetate timer box then start countdown
 function timerBox() {
     var timerB = document.createElement("section");
     timerB.className = "timer";
@@ -116,9 +129,13 @@ function timerBox() {
     countdown();
 }
 
+
+//Start countdown
 function countdown() {
+    //Set counter to 50 (45 with 5 second "Time Up" message)
     count = count + 50;
     var timer = contentE1.querySelector("#timer");
+    
     timeInterval = setInterval(function () {
         if (count > 6) {
             timer.innerHTML = "<p class='count-text'>" + (count - 5) + "</p>" + '<br>' + '<p>seconds remaining</p>';
@@ -130,34 +147,40 @@ function countdown() {
             timer.innerHTML = "Time Up!";
             count--;
         } else if (count < 0) {
-            count = 5;
+            //Count can get set to negative if -5 penalty is applied during "Time Up" message
+            count = 0;
         } else if (count === 0) {
             clearInterval(timeInterval);
             endGame();
         } else {
+            //If the game is completed and the timer is cleared, timer will never reach 0 and must be stopped
             clearInterval(timeInterval);
             return;
         }
     }, 1000);
 }
 
+//End the game by removing question area and timer, then running score and name entry boxes
 function endGame() {
-    //debugger;
     removeItems("qa");
     removeItems("timer");
     scoreDisplay();
     nameEntry();
 }
 
+//Display score box, calculated by current column and current time
 function scoreDisplay() {
     var scoreE1 = document.createElement("section");
     scoreE1.className = "final-score";
     scoreE1.setAttribute("id", "final-score");
     score = columnCounter + count;
+    //Clear timer
+    count = "";
     scoreE1.textContent = "Your Score: \r\n" + score;
     areaE1.appendChild(scoreE1);
 }
 
+//Display name entry box, create event listener for "Enter" key press
 function nameEntry() {
     var nameE1 = document.createElement("section");
     nameE1.className = "name-entry";
@@ -168,14 +191,15 @@ function nameEntry() {
 
     input.addEventListener("keyup", function(event) {
         if (event.key === 'Enter') {
+            //After enter key press, store the entered score, display localStorage score list, generate start button
             storeScore();
             checkScores();
-            count = "";
             startButton();
         }
     });
 }
 
+//Create score name based on already stored score items, store into local storage
  function storeScore() {
     i = 0;
     storageTest = localStorage.getItem("score" + i);
@@ -187,6 +211,7 @@ function nameEntry() {
     localStorage.setItem("score" + i, nameE2 + " " + score);
  }
 
+ //Create score ul, create title li, check scores stored in localStorage, generate li item for each score 
 function checkScores() {
     removeItems("final-score");
     removeItems("name-entry");
@@ -215,5 +240,5 @@ function checkScores() {
     } return;
 }
 
-
+//Call function to generate start button
 startButton();
